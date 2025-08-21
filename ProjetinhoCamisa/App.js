11aecdb3f778d445a1253a12a745b46f017c9b03
Duarte <dionysiomariaduarte2808@gmail.com>
2025-08-21 14:01:35 -0300

    App Modificado

--- ProjetinhoCamisa/App.js
+++ ProjetinhoCamisa/App.js
@@ -1,36 +1,45 @@
+import React from 'react';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+// Importação das telas
 import TelaInicial from './components/TelaInicial';
 import TelaLogin from './components/TelaLogin';
 import TelaCatalogo from './components/TelaCatalogo';
 import TelaDetalhes from './components/TelaDetalhes';
 
+// Criando a pilha de navegação
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      {/* Barra de status */}
+      {/* Configuração da barra de status */}
       <StatusBar barStyle="light-content" backgroundColor="#800000" />
 
+      {/* Definindo as telas da navegação */}
       <Stack.Navigator initialRouteName="Inicial">
-        {/* Tela de capa inicial */}
+        
+        {/* Tela de Boas-vindas */}
         <Stack.Screen
           name="Inicial"
           component={TelaInicial}
-          options={{ headerShown: false }} // sem barra no topo
+          options={{ headerShown: false }} // esconde o cabeçalho
         />
 
         {/* Tela de Login */}
         <Stack.Screen
           name="Login"
           component={TelaLogin}
-          options={{ headerShown: false }}
+          options={{
+            title: 'Login',
+            headerStyle: { backgroundColor: '#800000' },
+            headerTintColor: '#F5F5DC',
+          }}
         />
 
-        {/* Tela do Catálogo */}
+        {/* Tela de Catálogo */}
         <Stack.Screen
           name="Catalogo"
           component={TelaCatalogo}
